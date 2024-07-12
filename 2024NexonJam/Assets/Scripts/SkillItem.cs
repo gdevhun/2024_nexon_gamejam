a7f0cfbd74199d27f840103d263918e098317314
gdevhun <wjh9330@naver.com>
2024-07-13 02:48:07 +0900

    feat skillitem

---
+++ 2024NexonJam/Assets/Scripts/SkillItem.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+public enum Item
+{
+    Item1,
+    Item2,
+    Item3
+}
+
+public class SkillItem : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        //if(other.gameObject.GetComponent<Ball>)
+    }
+}
