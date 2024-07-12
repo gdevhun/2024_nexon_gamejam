d7b963484a3e54b8703fe3efff64bef7fef251eb
gdevhun <wjh9330@naver.com>
2024-07-12 20:21:38 +0900

    feat manager, menuscene

--- 2024NexonJam/Assets/Scripts/Managers/SoundManager.cs
+++ 2024NexonJam/Assets/Scripts/Managers/SoundManager.cs
@@ -1,106 +1,147 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using UnityEditor;
+using Cysharp.Threading.Tasks;
 using UnityEngine;
 
 public class SoundManager : SingletonBehaviour<SoundManager>
 {
-	[SerializeField] private AudioSource bgmSound; // 배경음 오디오
-	[SerializeField] private AudioClip[] bgmList, sfxList; // 배경음 및 효과음 리스트
-	private Dictionary<BgmType, AudioClip> _mapBgm = new Dictionary<BgmType, AudioClip>(); // (타입, 배경음) 맵핑
-	private Dictionary<SfxType, AudioClip> _mapSfx = new Dictionary<SfxType, AudioClip>(); // (타입, 효과음) 맵핑
-	[HideInInspector] public float bgmVolume, sfxVolume; // 배경음 볼륨 및 효과음 볼륨
-    [SerializeField] private GameObject player; // 플레이어
-    
-	private void Awake()
-    {
-        base.Awake();
-         // 볼륨 초기화
-         bgmVolume = 0.1f;
-         sfxVolume = 1f;
+	[System.Serializable]
+	public class Sound
+	{
+		public SoundType soundType;
+		public AudioClip clip;
+	}
 
-         // (타입, 배경음) 맵핑
-         Map();
+	[SerializeField]
+	private List<Sound> bgms = null;
+	[SerializeField]
+	private List<Sound> sfxs = null;
+	[SerializeField]
+	private AudioSource bgmPlayer = null;
+	public List<AudioSource> sfxPlayers = new List<AudioSource>();
 
-         // 배경음 재생
-         //BgmSoundPlay(BgmType.Lab);
-    }
+	private Dictionary<SoundType, AudioClip> _sfxDictionary;
 
-    // 테스트
-    private void Update()
-    {
-        //if(Input.GetKeyDown(KeyCode.Alpha8)) SFXPlay(SfxType.OpenDoor);
-    }
-    
-    // 배경음 및 효과음 맵핑
-    private void Map()
-    {
-        // (타입, 배경음) 맵핑
-        for(int i = 0; i < bgmList.Length; i++)
-        {
-            _mapBgm.Add((BgmType)i, bgmList[i]);
-        }
+	private Queue<AudioSource> _sfxQueue = new Queue<AudioSource>();
 
-        // (타입, 효과음) 맵핑
-        for(int i = 0; i < sfxList.Length; i++)
-        {
-            _mapSfx.Add((SfxType)i, sfxList[i]);
-        }
-    }
+	[HideInInspector]public float bgmVolume, sfxVolume; // 배경음 볼륨 및 효과음 볼륨
+	protected override void Awake()
+	{
+		base.Awake();
 
-    // 배경음
-    public void BgmSoundPlay(BgmType bgmType)
-    {
-        // 음원 할당
-        bgmSound.clip = _mapBgm[bgmType];
+		// SFX 딕셔너리 초기화
+		_sfxDictionary = sfxs.ToDictionary(s => s.soundType, s => s.clip);
+	}
 
-        // 음원 반복
-        bgmSound.loop = true;
+	private void Start()
+	{
+		bgmPlayer = gameObject.AddComponent<AudioSource>();
+		// 볼륨 초기화
+		bgmVolume = 0.6f; 
+		sfxVolume = 1f;
+		
+		// 메뉴bgm재생
+		PlayBGM(SoundType.MenuBGM);
 
-        // 음원 볼륨
-        bgmSound.volume = bgmVolume;
+		// SFX 플레이어 몇 개를 초기에 생성하고 리스트에 추가
+		for (int i = 0; i < 20; i++)
+		{
+			AudioSource sfxPlayer = gameObject.AddComponent<AudioSource>();
+			sfxPlayers.Add(sfxPlayer);
+			_sfxQueue.Enqueue(sfxPlayer);
+		}
+	}
 
-        // 음원 재생
-        bgmSound.Play();
-    }
+	public void PlayBGM(SoundType soundType)
+	{
+		var bgm = bgms.First(b => b.soundType == soundType);
+		bgmPlayer.clip = bgm.clip;
+		bgmPlayer.volume = bgmVolume; // 볼륨 조절 bgmVolume
+		bgmPlayer.loop = true;
+		bgmPlayer.Play();
+	}
 
-    // 효과음 
-    public void SFXPlay(SfxType type)
-    {
-        // 맵핑된 효과음 재생
-        if (_mapSfx.TryGetValue(type, out AudioClip clip))
-        {
-            AudioSource.PlayClipAtPoint(clip, Camera.main.transform.position, sfxVolume);
-        }
-    }
-
-    // 배경음 볼륨 조절
-    public void SetBgmVolume(float volume)
-    {
-        // 슬라이더 값에따라 볼륨 적용
-        bgmSound.volume = volume;
-
-        // 슬라이더 값을 변수에 저장해서 배경음악을 실행할때마다 볼륨을 지정
-        bgmVolume = volume;
-    }
-
-    // 효과음 볼륨 조절
-    public void SetSfxVolume(float volume)
-    {
-        // 슬라이더 값을 변수에 저장해서 효과음을 실행할때마다 볼륨을 지정
-        sfxVolume = volume;
-    }
-}
-
-// 재생할 배경음 타입 -> 키로 사용
-public enum BgmType
-{
+	public void StopBGM()
+	{
+		bgmPlayer.Stop();
+	}
 	
+	
+	// 사운드 플레이가 끝나면 호출하여 큐에 다시 넣기
+	public void ReturnSfxPlayerToQueue(AudioSource sfxPlayer)
+	{
+		_sfxQueue.Enqueue(sfxPlayer);
+	}
+	
+	// 사운드 재생이 끝나면 풀에 반환
+	private async UniTaskVoid ReturnSfxPlayerWhenFinished(AudioSource sfxPlayer, float delay)
+	{
+		// 사운드 재생 시간만큼 대기
+		await UniTask.Delay(TimeSpan.FromSeconds(delay));
+		
+		// 사운드 재생이 끝났으니 큐에 반환
+		ReturnSfxPlayerToQueue(sfxPlayer);
+	}
+	
+	public void PlaySfx(SoundType soundType)
+	{
+		if (_sfxDictionary.TryGetValue(soundType, out AudioClip clip))
+		{
+			AudioSource sfxPlayer = GetAvailableSfxPlayer();
+			sfxPlayer.clip = clip;
+			sfxPlayer.volume = sfxVolume; // 볼륨 조절 sfxVolume
+			sfxPlayer.Play();
+
+			// 사운드 재생이 끝나면 풀에 반환
+			ReturnSfxPlayerWhenFinished(sfxPlayer, clip.length).Forget();
+		}
+	}
+
+	private AudioSource GetAvailableSfxPlayer()
+	{
+		if (_sfxQueue.Count > 0)
+		{
+			return _sfxQueue.Dequeue();
+		}
+		else
+		{
+			// 새 플레이어를 생성하고 리스트에 추가
+			AudioSource newSFXPlayer = gameObject.AddComponent<AudioSource>();
+			sfxPlayers.Add(newSFXPlayer);
+			return newSFXPlayer;
+		}
+	}
+	
+	// 배경음 볼륨 조절
+	public void SetBgmVolume(float volume)
+	{
+		// 슬라이더 값에따라 볼륨 적용
+		bgmPlayer.volume = volume;
+
+		// 슬라이더 값을 변수에 저장해서 배경음악을 실행할때마다 볼륨을 지정
+		bgmVolume = volume;
+	}
+
+	// 효과음 볼륨 조절
+	public void SetSfxVolume(float volume)
+	{
+		// 슬라이더 값을 변수에 저장해서 효과음을 실행할때마다 볼륨을 지정
+		sfxVolume = volume;
+	}
+	public void PlayBtnSfx()
+	{
+		PlaySfx(SoundType.PlayerWalkSfx);
+	}
 }
 
-// 재생할 효과음 타입 -> 키로 사용
-public enum SfxType
+
+public enum SoundType
 {
+	MenuBGM = 0,
+	FirstBGM = 1,
+	SecondBGM = 2,
+	PlayerWalkSfx=3,
 	
 }
