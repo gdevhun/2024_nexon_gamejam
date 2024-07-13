daf9e1c858c6b82a80bf754c6dbd1e59f7ca2c35
gdevhun <wjh9330@naver.com>
2024-07-14 04:28:36 +0900

    feat playercon

---
+++ 2024NexonJam/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    //플레이어 바 이동
+    //플레이어 스킬 사용
+    private void Update()
+    {
+        
+    }
+}
